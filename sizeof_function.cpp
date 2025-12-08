65c0b8facd9f5a2fb7e3d0be31b2ccf983f6c97c
vinaysm-dvg <sm.vinay96@gmail.com>
2025-12-08 07:10:48 +0530

    Add files via upload

---
+++ sizeof_function.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("sizeof(int): %d\n", sizeof(int));
+	printf("sizeof(char): %d\n", sizeof(char));
+	printf("sizeof(double): %d\n", sizeof(double));
+	printf("sizeof(short): %d\n", sizeof(short));
+	printf("sizeof(long): %d\n", sizeof(long));
+	printf("sizeof(float): %d\n", sizeof(float));
+	printf("sizeof(long long): %d\n", sizeof(long long));
+	printf("All OK!");
+	return 0;
+}
